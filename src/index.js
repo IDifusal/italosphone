2d15767965cb187f08b13842583df36450563373
Cristhian Sanchez <52178427+IDifusal@users.noreply.github.com>
2020-09-27 21:47:38 -0500

    Frist Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+//Settings
+app.set("port",4000);
+app.set("views",path.join(__dirname,"views"));
+app.engine("html",require("ejs").renderFile);
+app.set("view engine","ejs");
+//routes
+app.use(require("./routes/index"));
+//midlewares
+
+//static files
+app.use(express.static(path.join(__dirname,"public")))
+//Server listeing
+app.listen(port, host, function() {
+    console.log("Server started.......PORT 3000");
+  });
